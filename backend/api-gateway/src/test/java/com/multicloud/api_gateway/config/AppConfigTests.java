b07f4c399afeb9752627f1b831806f382e5ec70b
ChellaVigneshKP <chellavigneshkp123@gmail.com>
2025-07-06 03:53:54 +0530

    config changes

--- backend/api-gateway/src/test/java/com/multicloud/api_gateway/config/AppConfigTests.java
+++ backend/api-gateway/src/test/java/com/multicloud/api_gateway/config/AppConfigTests.java
@@ -3,15 +3,21 @@ package com.multicloud.api_gateway.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Import(AppConfig.class)
 class AppConfigTests {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void restTemplateBeanIsNotNull() {
@@ -20,7 +26,8 @@ class AppConfigTests {
 
     @Test
     void restTemplateBeanIsSingleton() {
-        RestTemplate anotherRestTemplate = new AppConfig().template();
-        assertThat(restTemplate).isSameAs(anotherRestTemplate);
+        RestTemplate r1 = context.getBean(RestTemplate.class);
+        RestTemplate r2 = context.getBean(RestTemplate.class);
+        assertThat(r1).isSameAs(r2);
     }
 }
